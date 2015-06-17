26babfda7e90e9dc2549b124499dac7d26afd697
Yann Gouttenoire <yann.gouttenoire@u-psud.fr>
2015-06-17 17:41:51 +0200

    Plot 3 traj article 'plateau in ATI spectra'

--- ionization_in_strong_low-frequency_fields/ATI_spectra_chiral/system.cpp
+++ ionization_in_strong_low-frequency_fields/ATI_spectra_chiral/system.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 
 template<typename state_type>
-System<state_type>::System(ElectrostaticPotential<state_type> *myPotential, ElectricField &myField) : myPotential(myPotential), myField(myField) {}
+System<state_type>::System(ElectrostaticPotential<state_type> *myPotential, ElectricField *myField) : myPotential(myPotential), myField(myField) {}
 
 template<typename state_type>
     void System<state_type>::operator() (const state_type &x , state_type &dxdt , const double&  t)
@@ -23,9 +23,9 @@ template<typename state_type>
     dxdt[0]=x[3];
     dxdt[1]=x[4];
     dxdt[2]=x[5];
-    dxdt[3]=(*myPotential)('X',x)-myField('X',t);
-    dxdt[4]=(*myPotential)('Y',x)-myField('Y',t);
-    dxdt[5]=(*myPotential)('Z',x)-myField('Z',t);
+    dxdt[3]=(*myPotential)('X',x)-(*myField)('X',t);
+    dxdt[4]=(*myPotential)('Y',x)-(*myField)('Y',t);
+    dxdt[5]=(*myPotential)('Z',x)-(*myField)('Z',t);
     }
 
 
