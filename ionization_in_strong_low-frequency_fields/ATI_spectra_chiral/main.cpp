3faad363f62575dfc5b7c783638403072b31278c
Yann Gouttenoire <yann.gouttenoire@u-psud.fr>
2015-07-11 00:32:05 +0200

    Reduce weight ratio from 10000 to 10

--- ionization_in_strong_low-frequency_fields/ATI_spectra_chiral/main.cpp
+++ ionization_in_strong_low-frequency_fields/ATI_spectra_chiral/main.cpp
@@ -69,7 +69,7 @@ double desiredErrorMax=1E-12;
 double desiredErrorMin=desiredErrorMax/10.;
 
 //We declare a minimum threshold value for the probability of ionization
-double weightThresholdRatio=10000.;
+double weightThresholdRatio=10.;
 double weightThreshold;
 
 //We declare boolean controls
