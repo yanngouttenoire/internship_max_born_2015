3b74da2c2a423c0695a01880cdb74ebdf7c21ba1
Yann Gouttenoire <yann.gouttenoire@u-psud.fr>
2015-07-21 21:47:44 +0200

    New script 'paste.sh'

--- ionization_in_strong_low-frequency_fields/ATI_spectra_chiral/paste.sh
+++ ionization_in_strong_low-frequency_fields/ATI_spectra_chiral/paste.sh
@@ -7,18 +7,19 @@ touch temp.dat
 touch sum.dat
 touch col1.dat
 
-for file in leb_orient*.dat
+for file in X3*.dat
 do 
 awk  'BEGIN {ind=0}
-     $0==" " {ind=ind+1; indNbr=NR}
-     $1<30 && ind==0 {ypos[NR]=$2; print $1 >> "col1.dat"}
-     $1<30 && ind==2 {yneg[NR-indNbr]=$2}
+     NF!=0 && $1<30 && ind==0 {ypos[NR]=$2; print $1 > "col1.dat"}
+     $1<30 && ind==2 {yneg[NR-indNbr]=$2; print $1 >> "col1.dat"}
      $1==30 && ind==0 {linesNbr=NR-1}
+     !NF {ind=ind+1; indNbr=NR; print "" >> "col1.dat"}
      END {
      for(i=1; i<=linesNbr; i++) {
-     print ypos[i] >> "temp.dat"
+     print ypos[i] > "temp.dat"
      } 
-     printf("\n\n") >> "temp.dat"
+     print "" >> "temp.dat"
+     print "" >> "temp.dat"
      for(i=1; i<=linesNbr; i++) {
      print yneg[i] >> "temp.dat"
      } 
@@ -28,19 +29,18 @@ if [ -s "sum.dat" ]
 then
   paste -d " " sum.dat temp.dat > _sum_.dat
   mv _sum_.dat sum.dat  
-  rm temp.dat
 else
   cp temp.dat sum.dat
-  rm temp.dat
 fi
 
 done
 
-touch temp.dat
+rm temp.dat
+touch temp.dat 
 
 awk 'BEGIN {sum=0}
-     $0==" " {print("\n") >> "temp.dat"}
-     $0!=" " {for(i=1; i<=NF; i++) sum=sum+$i; print sum >> "temp.dat"; sum=0;}' sum.dat
+     !NF {print "" >> "temp.dat"}
+     NF!=0 {for(i=1; i<=NF; i++) sum=sum+$i; print sum >> "temp.dat"; sum=0;}' sum.dat
      
 paste -d " " col1.dat temp.dat > _sum_.dat
 
